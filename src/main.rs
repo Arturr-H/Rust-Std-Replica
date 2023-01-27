7f892e706dcd8c1b268c07567e082d9b402c624a
Artur <artur.h.0427@gmail.com>
2023-01-27 18:49:28 +0100

    Export `string` module - Later will be re-exported in `src/lib.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
+/* Global allowings */
+#![allow(dead_code)]
+
 /* Imports */
 mod hashmap;
+mod string;
 
 fn main() {
     let mut hm = hashmap::HashMap::new();
     hm.insert("tom", 15);
-    hm.insert("max", 12);
-    hm.insert("linda", 172);
-    hm.insert("bob", 111);
-    hm.insert("janice", 222);
-    hm.insert("haha", 333);
-    hm.insert("popo", 444);
-    hm.insert("baba", 555);
-    println!("{hm:?}");
-    println!("{:?}", hm.get("janice"));
+    println!("{:?}", hm.get("tom"));
+    let string = string::StdString::from("hello!");
+    println!("{}", &string[0..2]);
 }
