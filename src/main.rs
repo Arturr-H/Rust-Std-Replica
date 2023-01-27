1d528e61fb7a8428bd7f6fa3214de6499a31d9e0
Artur <artur.h.0427@gmail.com>
2023-01-27 13:08:20 +0100

    Import and use `src/hashmap.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 /* Imports */
+mod hashmap;
 
 fn main() {
+    let mut hm = hashmap::HashMap::new();
+    hm.insert("tom", 15);
+    hm.insert("max", 12);
+    hm.insert("linda", 172);
+    hm.insert("bob", 111);
+    hm.insert("janice", 222);
+    hm.insert("haha", 333);
+    hm.insert("popo", 444);
+    hm.insert("baba", 555);
+    dbg!(&hm.items);
+    dbg!(hm.get("tom"));
 }
